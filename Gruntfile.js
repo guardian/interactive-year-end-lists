08b82c832f5a5b41d696d0ac5db00c0a9c6988ee
andymason <andrew.mason@guardian.co.uk>
2014-03-12 12:11:50 +0000

    Added grunt tasks and build steps

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,20 +7,73 @@ module.exports = function(grunt) {
       server: {
         options: {
           port: 9000,
-          base: 'src',
-          keepalive: true
+          base: 'build'
         }
       }
     },
 
+    sass: {
+      build: {
+        options: {
+          includePaths: ['src/css/']
+        },
+        files: {
+          'build/css/main.css': 'src/css/main.scss'
+        }
+      }
+    },
+
+    clean: ['build/'],
+
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js']
+      files: ['Gruntfile.js', 'src/js/*.js', 'src/js/app/**/*.js']
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: './src/js/',
+          paths: {
+              'underscore': 'libs/underscore',
+              'jquery': 'libs/jquery',
+              'backbone': 'libs/backbone'
+          },
+          optimize: 'none',
+          name: 'main',
+          out: 'build/js/dreamteam.js'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['src/**/*.js'],
+        tasks: ['jshint', 'requirejs'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+
+    copy: {
+      build: {
+        files: [
+          { src: 'src/index.html', dest: 'build/index.html' },
+          { src: 'src/js/libs/require.js', dest: 'build/js/require.js' }
+        ]
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('default', ['connect']);
+  grunt.registerTask('build', ['jshint', 'clean', 'sass', 'requirejs', 'copy']);
+  grunt.registerTask('default', ['build', 'connect', 'watch']);
 };
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+require(['backbone', 'app/views/page-team'], function(backbone, PageTeamView) {
+    var pageTeamView = new PageTeamView();
+    console.log(pageTeamView);
+});
