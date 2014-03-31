9ad042bec69880f8c4e3eb1ab07562ad51b6c469
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-03-31 20:21:58 +0100

    Flesh out players.js a little with uids

--- src/js/app/data/players.js
+++ src/js/app/data/players.js
@@ -1,8 +1,14 @@
 define([], function() {
     return [
-        { name: 'andrew', country: 'UK', position: 'GL', imageSrc: '#'},
-        { name: 'sean', country: 'UK', position: 'FT', imageSrc: '#'},
-        { name: 'daan', country: 'UK', position: 'FT', imageSrc: '#'},
-        { name: 'chris', country: 'UK', position: 'DF', imageSrc: '#'}
+        { name: 'Ronaldo', uid: '1', country: 'UK', position: 'GL', imageSrc: '#'},
+        { name: 'Maradonna', uid: '2', country: 'UK', position: 'FT', imageSrc: '#'},
+        { name: 'Baggio', uid: '3', country: 'UK', position: 'FT', imageSrc: '#'},
+        { name: 'Banks', uid: '4', country: 'UK', position: 'GK', imageSrc: '#'},
+        { name: 'Beckenbauer', uid: '5', country: 'UK', position: 'DF', imageSrc: '#'},
+        { name: 'Cruijff', uid: '6', country: 'UK', position: 'DF', imageSrc: '#'},
+        { name: 'Eusebio', uid: '7', country: 'UK', position: 'DF', imageSrc: '#'},
+        { name: 'Klinsmann', uid: '8', country: 'UK', position: 'DF', imageSrc: '#'},
+        { name: 'Moore', uid: '9', country: 'UK', position: 'DF', imageSrc: '#'},
+        { name: 'Pele', uid: '10', country: 'UK', position: 'DF', imageSrc: '#'}
     ];
 });
