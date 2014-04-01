9dab55bdaba507c52e3a23c92b3dd08da03e1d16
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-04-01 12:28:39 +0100

    Adding test data for designing pitch with players on

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -23,10 +23,16 @@ define([
     App.opponent = new Backbone.Model();
 
     // Collections
-    App.usersTeamCollection = new TeamCollection();
-    App.opponentTeamCollection = new TeamCollection();
     App.playerCollection = new Backbone.Collection(PlayerData);
 
+    App.usersTeamCollection = new TeamCollection();
+    App.opponentTeamCollection = new TeamCollection();
+
+    // TODO: Adding test data for designing the pitch with positions
+    App.usersTeamCollection.add(PlayerData[1]);
+    App.usersTeamCollection.add(PlayerData[2]);
+    App.usersTeamCollection.add(PlayerData[3]);
+    
     // Views
     App.teamView = new TeamScreenView({ collection: App.playerCollection });
     App.matchView = new MatchScreenView();
