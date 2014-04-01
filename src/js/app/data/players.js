d23143a89bc5d54bd9d7b3b6c6a34ad42652c3ca
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-04-01 12:20:57 +0100

    Added apps/gls to player data store.

--- src/js/app/data/players.js
+++ src/js/app/data/players.js
@@ -3,15 +3,15 @@ define([], function() {
     var imagePath = '/images/player-';
 
     return [
-        { name: 'Ronaldo', uid: 1, country: 'UK', position: 'GL', imageSrc: imagePath + 'ronaldo.jpg'},
-        { name: 'Maradonna', uid: 2, country: 'UK', position: 'FT', imageSrc: imagePath + 'maradonna.jpg'},
-        { name: 'Baggio', uid: 3, country: 'UK', position: 'FT', imageSrc: imagePath + 'baggio.jpg'},
-        { name: 'Banks', uid: 4, country: 'UK', position: 'GK', imageSrc: imagePath + 'banks.jpg'},
-        { name: 'Beckenbauer', uid: 5, country: 'UK', position: 'DF', imageSrc: imagePath + 'beckenbauer.jpg'},
-        { name: 'Cruijff', uid: 6, country: 'UK', position: 'DF', imageSrc: imagePath + 'cruijff.jpg'},
-        { name: 'Eusebio', uid: 7, country: 'UK', position: 'DF', imageSrc: imagePath + 'eusebio.jpg'},
-        { name: 'Klinsmann', uid: 8, country: 'UK', position: 'DF', imageSrc: imagePath + 'klinsmann.jpg'},
-        { name: 'Moore', uid: 9, country: 'UK', position: 'DF', imageSrc: imagePath + 'moore.jpg'},
-        { name: 'Pele', uid: 10, country: 'UK', position: 'DF', imageSrc: imagePath + 'pele.jpg'}
+        { name: 'Ronaldo', apps: 98, gls: 62, uid: 1, country: 'Brazil', position: 'Striker', imageSrc: imagePath + 'ronaldo.jpg'},
+        { name: 'Maradonna', apps: 91, gls: 34, uid: 2, country: 'Argentina', position: 'Striker', imageSrc: imagePath + 'maradonna.jpg'},
+        { name: 'Baggio', apps: 56, gls: 27, uid: 3, country: 'Italy', position: 'Striker', imageSrc: imagePath + 'baggio.jpg'},
+        { name: 'Banks', apps: 73, gls: 0, uid: 4, country: 'UK', position: 'Goalkeeper', imageSrc: imagePath + 'banks.jpg'},
+        { name: 'Beckenbauer', apps: 103, gls: 14, uid: 5, country: 'Germany', position: 'Defender', imageSrc: imagePath + 'beckenbauer.jpg'},
+        { name: 'Cruyff', apps: 48, gls: 33, uid: 6, country: 'Holland', position: 'Midfield', imageSrc: imagePath + 'cruijff.jpg'},
+        { name: 'Eusebio', apps: 64, gls: 41, uid: 7, country: 'Portugal', position: 'Midfield', imageSrc: imagePath + 'eusebio.jpg'},
+        { name: 'Klinsmann', apps: 80, gls: 38, uid: 8, country: 'Germany', position: 'Striker', imageSrc: imagePath + 'klinsmann.jpg'},
+        { name: 'Moore', apps: 108, gls: 2, uid: 9, country: 'UK', position: 'Defender', imageSrc: imagePath + 'moore.jpg'},
+        { name: 'Pele', apps: 92, gls: 77, uid: 10, country: 'Brazil', position: 'Striker', imageSrc: imagePath + 'pele.jpg'}
     ];
 });
