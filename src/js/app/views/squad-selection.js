b4e597152c8beaebba38aa65d6f515994db473c9
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-04-01 12:19:57 +0100

    Filtering of players

--- src/js/app/views/squad-selection.js
+++ src/js/app/views/squad-selection.js
@@ -1,10 +1,12 @@
 // Squad selection
 define([
+    'app',
     'underscore',
     'backbone',
     'views/player',
     'text!templates/squad-selection.html'
 ], function(
+    App,
     _,
     Backbone,
     PlayerView,
@@ -21,23 +23,57 @@ define([
 
         template:  _.template(SquadSelectionTemplate),
 
-        initialize: function() {            
-            this.playerViews = [];
+        initialize: function() {
+            this.templateData = this.createFilterOptions();
             this.updatePlayerViews();
         },
 
+        createFilterOptions: function() {
+            var countries = [], positions = [];
+            App.playerCollection.each(function(player) {
+                if($.inArray(player.get('position'), positions) < 0) {
+                    positions.push(player.get('position'));
+                }
+                if($.inArray(player.get('country'), countries) < 0) {
+                    countries.push(player.get('country'));
+                }
+            });
+            var filterOptions = {};
+            if(countries) {
+                filterOptions.countries = countries.sort();
+            }
+            if(positions) {
+                filterOptions.positions = positions.sort();
+            }
+            return filterOptions;
+        },
+
         filterChange: function() {
-            console.log('Filter changed');
+            var filterOptions = {};
+            $('#player_filters select').each(function( index ) {
+                if($(this).val()) {
+                    filterOptions[$(this).data('filter-name')] = $(this).val();
+                }
+            });
+            this.updatePlayerViews(filterOptions);
         },
 
         resetTeam: function() {
+            this.updatePlayerViews();
+            $('#player_filters select').val('');
             return false;
         },
 
-        updatePlayerViews: function() {
-            this.collection.each(function(playerModel) {
+        updatePlayerViews: function(filterOptions) {
+            this.playerViews = [];
+            var playersFiltered = App.playerCollection;
+            if(!_.isEmpty(filterOptions)) {
+                playersFiltered = App.playerCollection.where(filterOptions);
+            }
+            playersFiltered.map(function(playerModel) {
                 this.playerViews.push(new PlayerView({ model : playerModel }));
             }, this);
+            this.renderPlayerViews();
         },
 
         renderPlayerViews: function() {
@@ -45,12 +81,12 @@ define([
             this.playerViews.forEach(function(playerView ) {
                 domContainer.appendChild( playerView.render().el );
             });
-            return domContainer;
+            this.$('#player_list').html(domContainer);
         },
 
         render: function() {
-            this.$el.html(this.template({}));
-            this.$('#player_list').append( this.renderPlayerViews() );
+            this.$el.html(this.template(this.templateData));
+            this.renderPlayerViews();
             return this;
         }
 
