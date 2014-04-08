1e7080f9a85e0aad83541a68531be1727fad68be
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-04-08 14:50:48 +0100

    Match screen populated
    User & team data fetched from Mongo.

--- src/js/app/views/match-screen.js
+++ src/js/app/views/match-screen.js
@@ -47,30 +47,32 @@ define([
         isReady: function() {
             if(this.userValidForMatch(App.player1) && this.userValidForMatch(App.player2)) {
 
-                var moments = {
-                    1: { 'time': 85, 'action': 'Goal', 'title': 'GOAL! 50 Shades of O’Shea 1-0 Arsenal (Naismith)', 'description': 'But it\'s a bad result now! What a brilliant individual goal! Outstanding!<br>PSG substitute Javier Pastore - a £37m man - takes a throw-in on the dead-ball line. He spins away from Cesar Azpilicueta, evades Frank Lampard with a Chris Waddle-style bodyswerve, does John Terry for pace and then fires in low past Petr Cech at his near post.' },
-                    2: { 'time': 85, 'action': 'Injury time', 'title': '', 'description': 'Four minutes added on in Paris. Not a bad result for Chelsea, I reckon. A 1-0 win at home and Jose\'s your uncle.' },
-                    3: { 'time': 85, 'action': 'Great save', 'title': '', 'description': 'Another Real free-kick, another flying save from Roman Weidenfeller! Gareth Bale has his eyes on the prize this time, unleashing a dipper that forces the Dortmund keeper into a fine save again.' },
-                    4: { 'time': 85, 'action': 'Yellow card', 'title': '', 'description': 'Javi Garcia hacks down Santi Cazorla. Yellow card and an Arsenal free-kick 40 yards from goal...' }
-                };
+                var team1 = App.player1TeamCollection.toJSON();
+                var team2 = App.player2TeamCollection.toJSON();
 
                 this.templateData = {
-                    player1: { details: App.player1.toJSON(), team: App.player1TeamCollection },
-                    player2: { details: App.player1.toJSON(), team: App.player2TeamCollection },
-                    moments: moments
+                    player1: { details: App.player1.toJSON(), team: team1 },
+                    player2: { details: App.player2.toJSON(), team: team2 }
                 };
+
+                var moments = {
+                    1: { 'time': 85, 'action': 'Goal', 'title': 'GOAL! 50 Shades of O’Shea 1-0 Arsenal (' + this.pickRandomPlayerName(team1) + ')', 'description': 'But it\'s a bad result now! What a brilliant individual goal! Outstanding!<br>PSG substitute ' + this.pickRandomPlayerName(team1) + ' - a £37m man - takes a throw-in on the dead-ball line. He spins away from Cesar Azpilicueta, evades Frank Lampard with a Chris Waddle-style bodyswerve, does John Terry for pace and then fires in low past Petr Cech at his near post.' },
+                    2: { 'time': 85, 'action': 'Injury time', 'title': '', 'description': 'Four minutes added on in Paris. Not a bad result for Chelsea, I reckon. A 1-0 win at home and Jose\'s your uncle.' },
+                    3: { 'time': 85, 'action': 'Great save', 'title': '', 'description': 'Another Real free-kick, another flying save from Roman Weidenfeller! ' + this.pickRandomPlayerName(team2) + ' has his eyes on the prize this time, unleashing a dipper that forces the Dortmund keeper into a fine save again.' },
+                    4: { 'time': 85, 'action': 'Yellow card', 'title': '', 'description':  this.pickRandomPlayerName(team2) + ' hacks down Santi Cazorla. Yellow card and an Arsenal free-kick 40 yards from goal...' }
+                };
+
+                this.templateData.moments = moments;
 
                 this.render();
             }
 
         },
 
-        loadingRender: function() {
-
-        },
-
-        matchResultRender: function() {
-
+        pickRandomPlayerName: function (obj) {
+            var keys = Object.keys(obj);
+            var player = obj[keys[ keys.length * Math.random() << 0]];
+            return player.name;
         },
 
         userValidForMatch: function(player) {
@@ -95,7 +97,18 @@ define([
         validateTeamSelection: function(player) {
             var res = { status: 'fail', message: '' };
             if(player.get('teamSelection')) {
-                if(player.get('teamSelection').split(',').length < 11) {
+                var selection = player.get('teamSelection').split(',');
+                if(selection.length < 11) {
+
+                    selection.map(function(playerUID) {
+                        var playerModel = App.playerCollection.findWhere({'uid': parseInt(playerUID)});
+                        if(player.get('startingUser') == 1) {
+                            App.player1TeamCollection.add(playerModel);
+                        } else {
+                            App.player2TeamCollection.add(playerModel);
+                        }
+                    });
+
                     res.status = 'success';
                 } else {
                     res.message = 'ID ' + player.get('guardianID') + ': not enough players!';
