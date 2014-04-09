a429b53e6cb33168e7ad020c29eb869fea119ba7
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-04-09 11:22:02 +0100

    Using new fetchByGuardianId()
    Old method leaving out teamSelection var

--- src/js/app/routes.js
+++ src/js/app/routes.js
@@ -15,10 +15,10 @@ function(
 
         showMatch: function(player1id, player2id) {
 
-            App.player1.set({guardianID: player1id}).fetch();
+            App.player1.set({guardianID: player1id}).fetchByGuardianId();
             App.player1.set('startingUser', 1);
 
-            App.player2.set({guardianID: player2id}).fetch();
+            App.player2.set({guardianID: player2id}).fetchByGuardianId();
             App.player2.set('startingUser', 2);
 
             App.$el.empty();
