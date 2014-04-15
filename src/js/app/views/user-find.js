1b85b631ff85e3ce6a18030584958706d2f613cd
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-04-15 15:17:34 +0100

    Shows all users on user page.

--- src/js/app/views/user-find.js
+++ src/js/app/views/user-find.js
@@ -11,14 +11,35 @@ define([
 ) {
     return Backbone.View.extend({
 
+        id: 'users-find-inner',
         template: _.template(UserFindTemplate),
 
+        events: {
+            'click #playAgainst': 'playAgainst'
+        },
+
         initialize: function () {
-            // Search for all users and show them in the view with an option to play against them
+            this.templateData = {users: null};
+            this.getAllUsers();
+        },
+
+        playAgainst: function (e) {
+            App.appRoutes.navigate('/match/' + App.userDetails.get('guardianID') + '/' + $(e.target).data('guardian-id'), { trigger: true });
+        },
+
+        getAllUsers: function () {
+            var templateData = {users: null};
+            var thisView = this;
+            $.ajax({
+                url: "http://ec2-54-195-231-244.eu-west-1.compute.amazonaws.com/allusers"
+            }).done(function (data) {
+                thisView.templateData.users = data;
+                thisView.render();
+            });
         },
 
         render: function () {
-            this.$el.html(this.template());
+            this.$el.html(this.template(this.templateData));
             return this;
         }
 
