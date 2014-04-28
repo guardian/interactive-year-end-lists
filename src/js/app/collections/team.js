27adaf7c2ead268f687e576285c432bd5d710877
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-04-28 18:46:52 +0100

    Relax validation on player positions.

--- src/js/app/collections/team.js
+++ src/js/app/collections/team.js
@@ -16,56 +16,14 @@ define([
             var res = {
                 status: 'fail',
                 message: ''
-            },
-                // Cant have more than x players in position y
-                positionsValidation = {
-                    'ST': {
-                        allowed: 2,
-                        name: 'Strikers'
-                    },
-                    'ML': {
-                        allowed: 1,
-                        name: 'Left midfielder'
-                    },
-                    'MR': {
-                        allowed: 1,
-                        name: 'Right midfielder'
-                    },
-                    'MC': {
-                        allowed: 2,
-                        name: 'Centre midfielders'
-                    },
-                    'RB': {
-                        allowed: 1,
-                        name: 'Right back'
-                    },
-                    'LB': {
-                        allowed: 1,
-                        name: 'Left back'
-                    },
-                    'CB': {
-                        allowed: 2,
-                        name: 'Centre backs'
-                    },
-                    'GK': {
-                        allowed: 1,
-                        name: 'Goalkeeper'
-                    }
-                };
+            };
 
             // Cannot already be in squad
             if (!App.usersTeamCollection.contains(model)) {
 
                 // Cant have more than 11 players
                 if ((App.usersTeamCollection.length) <= 11) {
-
-                    if ((App.usersTeamCollection.where({
-                            'position': model.get('position')
-                        }).length + 1) <= positionsValidation[model.get('position')].allowed) {
-                        res.status = 'success';
-                    } else {
-                        res.message = 'Cant have more than ' + positionsValidation[model.get('position')].allowed + ' ' + positionsValidation[model.get('position')].name;
-                    }
+                    res.status = 'success';
                 } else {
                     res.message = 'Cant have more than 11 players!';
                 }
