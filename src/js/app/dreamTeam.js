8a9f1de32e17122469085ec4932b9964ea5e2f1d
Daniel Levitt <daniel.levitt@guardian.co.uk>
2014-04-30 11:20:17 +0100

    Commenting dreamTeam.js

--- src/js/app/dreamTeam.js
+++ src/js/app/dreamTeam.js
@@ -26,44 +26,46 @@ define([
     Routes
 ) {
 
-    // Models
+    /**
+     * Create user model and load Guardian identity toolkit,
+     * the toolkit might be R2 or Next-Gen
+     */
     App.userDetails = new UserModel();
     App.userDetails.setToolKitObject();
 
+    // Once toolkit is ready
     Backbone.on('toolkitReady', function () {
 
-        console.log('Loaded: tookitObject');
-
-        console.log(App.toolkitObj);
-
-        App.userDetails.checkUserStatus(); // Automatic login
+        // Automatic login
+        App.userDetails.checkUserStatus();
 
         App.viewingPlayer = new UserModel();
         App.player1 = new UserModel();
         App.player2 = new UserModel();
 
         // Variables for listeners
-        App.playerSelected = new Backbone.Model(); // Opens the player card
+        App.playerSelected = new Backbone.Model(); // Opens the player modal
         App.visualPrompt = new Backbone.Model(); // Shows a prompt to the user (loading etc)
 
         // Collections
         App.playerCollection = new PlayersCollection();
 
+        // TODO: Switch from Google spreadsheet
         App.playerCollection.fetchGoogleData();
-        // App.playerCollection.fetchLocalData(); // TODO: Enable in prod
+        // App.playerCollection.fetchLocalData();
 
         App.matchModel = new MatchModel();
         App.createMatch = new CreateMatchView();
 
+        // Once user and player data is loaded
         Backbone.on('dataReady', function () {
-
-            console.log('All done. Render all');
 
             App.usersTeamCollection = new TeamCollection();
             App.viewingPlayerTeamCollection = new TeamCollection();
             App.player1TeamCollection = new TeamCollection();
             App.player2TeamCollection = new TeamCollection();
 
+            // Load the current users team
             App.userDetails.fetchUserTeamFromStorage();
 
             // Views
@@ -80,6 +82,7 @@ define([
 
     });
 
+    // Add CSS
     $('head').append('<link rel="stylesheet" href="@@assetPathcss/vendor.css" type="text/css" />');
     $('head').append('<link rel="stylesheet" href="@@assetPathcss/main.css" type="text/css" />');
 
@@ -88,7 +91,6 @@ define([
      * @param  {element} el DOM element provided from the page ie. <figure>
      */
     function boot(el, config) {
-
         App.$el = $(el);
     }
 
