69db980e718c325e33cdb83596e101d8e666a3ce
daanlouter <daan.louter@gmail.com>
2014-05-01 10:48:06 +0100

    styled structure of match and user page

--- src/js/app/views/match.js
+++ src/js/app/views/match.js
@@ -91,7 +91,7 @@ define([
 
             this.renderTeams();
             this.renderMatchHistory(matchDetails[1].guardianID, matchDetails[2].guardianID);
-
+            
             return this;
         }
 
