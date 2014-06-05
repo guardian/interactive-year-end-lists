82b5e20ab24825092238f2d805e4c28dedb79b00
andymason <andrew.mason@guardian.co.uk>
2014-06-05 10:13:48 +0100

    Removed header

--- src/js/app/dreamTeam.js
+++ src/js/app/dreamTeam.js
@@ -104,6 +104,9 @@ define([
      * @param  {element} el DOM element provided from the page ie. <figure>
      */
     function boot(el, config) {
+        // Hide header
+        $('.content__head .gs-container').hide();
+
         App.$el = $(el);
         // Check CORS support IE10+, Chrome, Safari, FF
         if ($.support.cors === false) {
