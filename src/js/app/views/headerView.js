e998fe7e18a2621e6f87d1fc3c7719b0d3fecaf4
andymason <andrew.mason@guardian.co.uk>
2014-06-24 16:25:28 +0100

    Hooked up header to routes

--- src/js/app/views/headerView.js
+++ src/js/app/views/headerView.js
@@ -1,8 +1,9 @@
 define([
     'backbone',
     'underscore',
+    'routes',
     'text!templates/headerTemplate.html'
-], function(Backbone, _, templateHTML) {
+], function(Backbone, _, routes, templateHTML) {
     'use strict';
 
     return Backbone.View.extend({
@@ -13,8 +14,35 @@ define([
 
         template: _.template(templateHTML),
 
+        initialize: function() {
+            // Listen to routes
+            routes.on('route:default', this.setDefault, this);
+            routes.on('route:singleGame', this.setSingleGame, this);
+            routes.on('route:catalogue', this.setCatalogue, this);
+        },
+
+        setDefault: function() {
+            console.log('here');
+           this.$links.removeClass('active');
+           this.$home.addClass('active');
+        },
+
+        setSingleGame: function() {
+           this.$links.removeClass('active');
+           this.$single.addClass('active');
+        },
+
+        setCatalogue: function() {
+           this.$links.removeClass('active');
+           this.$catalogue.addClass('active');
+        },
+
         render: function() {
             this.$el.html(this.template());
+            this.$links = this.$('.link');
+            this.$home = this.$('.link_home'); 
+            this.$catalogue = this.$('.link_catalogue'); 
+            this.$single = this.$('.link_single'); 
             return this;
         }
     });
